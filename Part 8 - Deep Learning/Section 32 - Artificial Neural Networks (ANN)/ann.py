751a9eadacf9fa715cecd3583b2e3caa92563b45
ZhaoHuiXin <zhaohuxin@autoforce.net>
2019-05-20 09:29:01 +0800

    fill the leave out

--- Part 8 - Deep Learning/Section 32 - Artificial Neural Networks (ANN)/ann.py
+++ Part 8 - Deep Learning/Section 32 - Artificial Neural Networks (ANN)/ann.py
@@ -83,4 +83,15 @@ classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 
 
 # Part 3 - Making the predictions and evaluating the model
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
+
+
 
